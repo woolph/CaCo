5d2d50992d2f7093d672647fa293acaa6ff4328b
Wolfgang Mayer <wolfgang.mayer@engel.at>
2019-10-01 08:44:53 +0200

    fixed bug in arena collection view (showing the correct possessions)

--- src/main/kotlin/at/woolph/caco/view/collection/ArenaCollectionView.kt
+++ src/main/kotlin/at/woolph/caco/view/collection/ArenaCollectionView.kt
@@ -1,21 +1,16 @@
 package at.woolph.caco.view.collection
 
-import at.woolph.caco.datamodel.collection.CardLanguage
-import at.woolph.caco.datamodel.sets.Card
-import at.woolph.caco.datamodel.sets.CardSet
 import at.woolph.caco.importer.collection.importArenaCollection
 import javafx.scene.control.ToolBar
-import org.jetbrains.exposed.sql.transactions.transaction
 import tornadofx.action
 import tornadofx.button
-import tornadofx.observable
 
 class ArenaCollectionView: CollectionView(COLLECTION_SETTINGS) {
 	companion object {
 		val COLLECTION_SETTINGS = CollectionSettings(4, 0,
 				{ it.cards.any { it.arenaId != null } },
-				{ it.possessions.filter { it.language == CardLanguage.ENGLISH && !it.foil.isFoil }.count() },
-				{ it.possessions.filter { it.language == CardLanguage.ENGLISH && it.foil.isFoil }.count() })
+				{ it.arenaPossessions.count() },
+				{ it.arenaPossessions.count() })
 	}
 
     override fun CardPossessionModel.filterView(): Boolean = arenaId != null
