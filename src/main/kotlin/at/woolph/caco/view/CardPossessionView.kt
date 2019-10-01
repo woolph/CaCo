4a452c028b7194222e7b7e5e890b5fb43020ef5c
Wolfgang Mayer <wolfgang.mayer@engel.at>
2019-10-01 10:45:08 +0200

    enhanced possession view

--- src/main/kotlin/at/woolph/caco/view/CardPossessionView.kt
+++ src/main/kotlin/at/woolph/caco/view/CardPossessionView.kt
@@ -4,19 +4,24 @@ import at.woolph.caco.Styles
 import at.woolph.caco.datamodel.collection.CardCondition
 import at.woolph.caco.datamodel.collection.CardLanguage
 import at.woolph.caco.view.collection.CardPossessionModel
-import at.woolph.libs.ktfx.mapBinding
 import at.woolph.libs.ktfx.toStringBinding
-import javafx.beans.binding.Bindings
-import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleObjectProperty
-import javafx.collections.ObservableIntegerArray
+import javafx.geometry.Pos
+import javafx.geometry.Rectangle2D
 import javafx.scene.control.Label
-import org.jetbrains.exposed.sql.transactions.transaction
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import tornadofx.Fragment
 import tornadofx.*
 
 fun <K,V> generateMap(keys: Collection<K>, valueGenerator: (K)->V) = keys.map { it to valueGenerator(it) }.toMap()
 fun <K,V> generateMap(keys: Array<K>, valueGenerator: (K)->V) = keys.map { it to valueGenerator(it) }.toMap()
+
+class IconCollection(val image: Image, val iconWidth: Double, val iconHeight: Double) {
+	operator fun get(row: Int, column: Int) = ImageView(image).apply {
+		viewport = Rectangle2D(iconWidth*column,iconHeight*row,iconWidth, iconHeight)
+	}
+}
 
 class CardPossessionView(val cardProperty: SimpleObjectProperty<CardPossessionModel?> = SimpleObjectProperty(null)) : Fragment() {
 	//var card by cardProperty
@@ -29,7 +34,35 @@ class CardPossessionView(val cardProperty: SimpleObjectProperty<CardPossessionMo
 		}
 	private val sums = generateMap(languages) { integerBinding(this, *possessions[it]?.values?.toTypedArray() ?: emptyArray()) { possessions[it]?.values?.sumBy { it.value } ?: 0 } }
 
+	private val iconCollection = IconCollection(resources.image("mkm-icons.png"), 16.0, 16.0)
+
+	private fun CardLanguage.getIcon() = when(this) {
+		CardLanguage.ENGLISH -> iconCollection[5,1]
+		CardLanguage.FRENCH -> iconCollection[5,3]
+		CardLanguage.GERMAN -> iconCollection[5,5]
+		CardLanguage.SPANISH -> iconCollection[5,7]
+		CardLanguage.ITALIAN -> iconCollection[5,9]
+		CardLanguage.CHINESE -> iconCollection[5,11]
+		CardLanguage.JAPANESE -> iconCollection[5,13]
+		CardLanguage.PORTUGUESE -> iconCollection[5,15]
+		CardLanguage.RUSSIAN -> iconCollection[5,17]
+		CardLanguage.KOREAN -> iconCollection[5,19]
+		CardLanguage.CHINESE_TRADITIONL -> iconCollection[5,21]
+		else -> Label(this.toString())
+	}
+
+	private fun CardCondition.getIcon() = when(this) {
+		CardCondition.NEAR_MINT -> iconCollection[4,2]
+		CardCondition.EXCELLENT -> iconCollection[4,4]
+		CardCondition.GOOD -> iconCollection[4,6]
+		CardCondition.PLAYED -> iconCollection[4,8]
+		CardCondition.POOR -> iconCollection[4,10]
+		else -> Label(this.toString())
+	}
+
 	override val root = gridpane {
+		minWidth = 300.0
+		prefWidth = 300.0
 		addClass(Styles.cardPossessionView)
 
 		paddingAll = 10.0
@@ -39,27 +72,38 @@ class CardPossessionView(val cardProperty: SimpleObjectProperty<CardPossessionMo
 		row {
 			label("")
 			languages.forEach { cl ->
-				label(cl.toString())
+				add(cl.getIcon())
 			}
 		}
 		row {
-			label("\u2211")
+			label("\u2211") {
+				maxWidth = Double.POSITIVE_INFINITY
+				alignment = Pos.CENTER
+			}
 			languages.forEach { cl ->
 				label {
 					textProperty().bind(sums[cl]?.toStringBinding())
+					maxWidth = Double.POSITIVE_INFINITY
+					alignment = Pos.CENTER_RIGHT
 				}
 			}
 		}
 		conditions.forEach { cc ->
 			row {
-				label(cc.toString())
+				add(cc.getIcon())
 
 				languages.forEach { cl ->
 					label {
 						textProperty().bind(possessions[cl]?.get(cc)?.toStringBinding())
+						maxWidth = Double.POSITIVE_INFINITY
+						alignment = Pos.CENTER_RIGHT
 					}
 				}
 			}
+		}
+		constraintsForColumn(0).percentWidth = 150.0/(languages.count()+1.5)
+		for(columnIndex in 1..languages.count()) {
+			constraintsForColumn(columnIndex).percentWidth = 100.0/(languages.count()+1.5)
 		}
 	}
 
