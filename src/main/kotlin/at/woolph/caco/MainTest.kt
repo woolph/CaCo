3c731c7b451f99a5a309b0807239c27dc3a3bda0
Wolfgang Mayer <wolfgang.mayer@engel.at>
2020-10-16 07:46:21 +0200

    enhanced deck import from deckbox.org

--- src/main/kotlin/at/woolph/caco/MainTest.kt
+++ src/main/kotlin/at/woolph/caco/MainTest.kt
@@ -21,29 +21,33 @@ fun main(args: Array<String>) {
 			println(link)
 
 			val deckDoc = Jsoup.connect(link).get()
-			val commander = deckDoc.selectFirst("div.deck_info_widget").let {
-				val regexCommander = Regex("<div class=\"half_min_page indented_content\"> <span class=\"dt note\">Commander:</span>\\s*<div class=\"card inline_block\">\\s*<a class=\"simple\" href=\"[^\"]*\" target=\"_blank\">(.*)</a>")
-				regexCommander.find(it.html())?.groups?.get(1)?.value // TODO commander eleganter herauslesen
+			val (format, commander) = deckDoc.selectFirst("div.deck_info_widget").let {
+				it.selectFirst("span:matches(Format:) ~ span.variant")?.text() to it.selectFirst("span:matches(Commander:) ~ div > a")?.text()
 			}
-			println("Commander:")
-			println("1 $commander")
+			val mainboard = deckDoc.selectFirst("table.main").select("tr[id]").mapNotNull {
+				val count = it.selectFirst("td.card_count")?.text()
+				val cardName = it.selectFirst("td.card_name")?.text()
+
+				if(cardName != commander) cardName to count else null
+			}.associate { it }
+			val sideboard = deckDoc.selectFirst("table.sideboard").select("tr[id]").mapNotNull {
+				val count = it.selectFirst("td.card_count").text()
+				val cardName = it.selectFirst("td.card_name a").text()
+
+				if(cardName != commander) cardName to count else null
+			}.associate { it }
+
+			println("Format: $format")
+			if (format == "com" || format == "bra" || format == "oat") {
+				println("Commander:")
+				println("1 $commander")
+			}
 
 			println("Mainboard:")
-			deckDoc.selectFirst("table.main").select("tr[id]").forEach {
-				val count = it.selectFirst("td.card_count").text()
-				val cardName = it.selectFirst("td.card_name").selectFirst("a").text()
+			mainboard.forEach { (cardName, count) -> println("$count $cardName") }
 
-				if(cardName != commander)
-					println("$count $cardName")
-			}
-
-			println(if(commander != null) "Maybeboard:" else "Sideboard:")
-			deckDoc.selectFirst("table.sideboard").select("tr[id]").forEach {
-				val count = it.selectFirst("td.card_count").text()
-				val cardName = it.selectFirst("td.card_name").selectFirst("a").text()
-
-				println("$count $cardName")
-			}
+			println(if (format == "com" || format == "bra" || format == "oat") "Maybeboard:" else "Sideboard:")
+			sideboard.forEach { (cardName, count) -> println("$count $cardName") }
 
 			print("Press Enter to continue: ")
 			readLine()
