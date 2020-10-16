eccbb6bf5a888d392598fe312d4f871590eaa0e5
Wolfgang Mayer <wolfgang.mayer@engel.at>
2020-10-16 07:50:26 +0200

    prototyping BulkAdditionDialog enhancement: Excel like behavior (TAB + ENTER on CellEdit) *UNFINISHED*

--- src/main/kotlin/at/woolph/caco/view/collection/BulkAdditionDialog.kt
+++ src/main/kotlin/at/woolph/caco/view/collection/BulkAdditionDialog.kt
@@ -1,9 +1,12 @@
 package at.woolph.caco.view.collection
 
-import at.woolph.caco.datamodel.collection.CardPossession
 import at.woolph.caco.datamodel.collection.CardCondition
 import at.woolph.caco.datamodel.collection.CardLanguage
-import at.woolph.caco.datamodel.sets.*
+import at.woolph.caco.datamodel.collection.CardPossession
+import at.woolph.caco.datamodel.sets.Card
+import at.woolph.caco.datamodel.sets.CardSet
+import at.woolph.caco.datamodel.sets.Foil
+import at.woolph.caco.datamodel.sets.Rarity
 import at.woolph.caco.importer.collection.toLanguageDeckbox
 import at.woolph.caco.view.CardDetailsView
 import at.woolph.caco.view.getCachedImage
@@ -16,11 +19,30 @@ import javafx.collections.FXCollections
 import javafx.event.EventHandler
 import javafx.scene.control.*
 import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Priority
 import javafx.stage.FileChooser
 import org.jetbrains.exposed.sql.transactions.transaction
 import tornadofx.*
+
+
+fun <S> TableView<S>.getNextEditableColumn(forward: Boolean, currentCol: TableColumn<S, out Any>?): TableColumn<S, *>? {
+	val columns = this.columns.filter { it.isEditable && it.isVisible && (it.styleClass.contains("editable-col") || it.styleClass.contains("result-col")) }.toList()
+
+	if (columns.size < 2) {
+		return null
+	}
+
+	val currentIndex = columns.indexOf(currentCol)
+
+	val nextIndex = if (forward) {
+			if (currentIndex >= columns.size - 1) 0 else currentIndex+1
+		} else {
+			if (currentIndex <= 0) columns.size - 1 else currentIndex-1
+		}
+	return columns[nextIndex]
+}
 
 class BulkAdditionDialog(val set: CardSet, val owner: View, imageLoading: Boolean): Dialog<Boolean>() {
 
@@ -117,6 +139,9 @@ class BulkAdditionDialog(val set: CardSet, val owner: View, imageLoading: Boolea
                 && (filterRarityMythic || cardInfo.rarity.value != Rarity.MYTHIC)
         }
     }
+
+	var lastKeyEvent: KeyEvent? = null
+	var editingPosition: TablePosition<CardModel, *>? = null
 
     init {
         initOwner(owner.primaryStage)
@@ -261,10 +286,12 @@ class BulkAdditionDialog(val set: CardSet, val owner: View, imageLoading: Boolea
                             vGrow = Priority.ALWAYS
                         }
 
-                        column("CardSet Number", CardModel::numberInSet) {
+                        column("#", CardModel::numberInSet) {
+							tooltip("Collector Number")
                             contentWidth(5.0, useAsMin = true, useAsMax = true)
                         }
-                        column("Rarity", CardModel::rarity) {
+                        column("R", CardModel::rarity) {
+							tooltip("Rarity")
                             contentWidth(5.0, useAsMin = true, useAsMax = true)
                         }
 
@@ -272,11 +299,79 @@ class BulkAdditionDialog(val set: CardSet, val owner: View, imageLoading: Boolea
 
                         column("Add", CardModel::bulkAdditionNonPremium) {
                             contentWidth(5.0, useAsMin = true, useAsMax = true)
+							makeEditable()
                         }
 
                         column("Add Premium", CardModel::bulkAdditionPremium) {
                             contentWidth(5.0, useAsMin = true, useAsMax = true)
+							makeEditable()
                         }
+
+						column("Add Prerelease Promo", CardModel::bulkAdditionPrereleasePromo) {
+							contentWidth(5.0, useAsMin = true, useAsMax = true)
+							makeEditable()
+						}
+
+
+						addEventFilter(KeyEvent.KEY_PRESSED) {
+							if (it.code == KeyCode.TAB || it.code == KeyCode.ENTER) {
+								println("TAB or ENTER down on tableView")
+								editingPosition = editingCell
+								lastKeyEvent = it
+
+								this.edit(0, getNextEditableColumn(it.isShiftDown, editingCell?.tableColumn))
+								it.consume()
+							}
+//							when(it.code) {
+//								KeyCode.TAB -> {
+//									selectionModel.selectedCells.singleOrNull()?.let { currentCell ->
+//										selectionModel.clearAndSelect(currentCell.row)
+//										getNextEditableColumn(it.isShiftDown, editingCell?.tableColumn)
+//									}
+//								}
+//							}
+						}
+
+						setOnMouseReleased { editingPosition = null }
+						columns.forEach {
+							it.setOnEditCancel {
+								editingPosition = null
+							}
+							it.setOnEditCommit {
+								val editingPosition2 = editingPosition
+								val lastKeyEvent = lastKeyEvent
+								if (editingPosition2 != null && lastKeyEvent != null) {
+									if (lastKeyEvent.code == KeyCode.ENTER) {
+										var selectIdx: Int = if (lastKeyEvent.isShiftDown) {
+											editingPosition2.row - 1
+										} else {
+											editingPosition2.row + 1
+										}
+										if (selectIdx < 0) {
+											selectIdx = items.size - 1
+										} else if (selectIdx > items.size - 1) {
+											selectIdx = 0
+										}
+										layout()
+										scrollTo(if (selectIdx == 0) selectIdx else selectIdx - 1)
+										selectionModel.clearAndSelect(selectIdx)
+										edit(selectIdx, editingPosition2.tableColumn)
+									} else if (lastKeyEvent.code == KeyCode.TAB) {
+										getNextEditableColumn(!lastKeyEvent.isShiftDown, editingPosition2.tableColumn)?.let { colToEdit ->
+											layout()
+											scrollToColumn(colToEdit)
+											edit(editingPosition2.getRow(), colToEdit)
+										}
+									}
+
+									editingPosition = null
+								}
+							}
+						}
+
+
+						enableCellEditing()
+						regainFocusAfterEdit()
 
                         selectionModel.selectionMode = SelectionMode.SINGLE
                         selectionModel.selectedItemProperty().addListener { _, _, _ ->
