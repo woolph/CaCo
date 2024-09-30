35ecc314106da894c781c0747887ccb74d54f041
Wolfgang Mayer <wolfgang.mayer@engel.at>
2024-09-30 15:15:05 +0200

    deck diff

--- src/main/kotlin/at/woolph/caco/importer/deck/DeckDiff.kt
+++ src/main/kotlin/at/woolph/caco/importer/deck/DeckDiff.kt
@@ -2,6 +2,7 @@ package at.woolph.caco.importer.deck
 
 import at.woolph.caco.cli.DeckList
 import at.woolph.caco.datamodel.decks.Format
+import com.github.ajalt.mordant.table.table
 import com.github.ajalt.mordant.terminal.Terminal
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
@@ -21,24 +22,30 @@ class DeckDiff(
         DeckboxDeckImporter().importDeckboxDecks("woolph").take(20).filter { it.format == Format.Commander }.mapNotNull { deck ->
             archidektDecks.await().find { it.commandZone == deck.commandZone }?.let { Pair(deck, it) }
         }.collect { (deck1, deck2) ->
-            terminal.println("Diffing decks \"${deck1.commandZone.keys.joinToString(" & ") { it }}\"")
             diffDeck(deck1, deck2)
         }
     }
 
     fun diffDeck(deck1: DeckList, deck2: DeckList) {
-        deck1.mainboard.filter { it.key !in deck2.mainboard.keys }.forEach {
-            terminal.println("Card ${it} is in deck1 but not in deck2")
-        }
-        deck2.mainboard.filter { it.key !in deck1.mainboard.keys }.forEach {
-            terminal.println("Card ${it} is in deck2 but not in deck1")
-        }
-        deck1.mainboard
-            .mapNotNull { deck2.mainboard[it.key]?.let { deck2Value -> Triple(it.key, it.value, deck2Value) } }
-            .filter{ (_, value1, value2) -> value1 != value2 }
-            .forEach { (card, value1, value2) ->
-                terminal.println("Card ${card} is $value1 times in deck1 but $value2 times in deck2")
+        terminal.println(table {
+            captionTop("Diffing decks \"${deck1.commandZone.keys.joinToString(" & ") { it }}\"")
+            body {
+
+                deck1.mainboard.filter { it.key !in deck2.mainboard.keys }.forEach {
+                    row("${it.value} ${it.key}", "")
+                }
+                deck2.mainboard.filter { it.key !in deck1.mainboard.keys }.forEach {
+                    row("", "${it.value} ${it.key}")
+                }
+                deck1.mainboard
+                    .mapNotNull { deck2.mainboard[it.key]?.let { deck2Value -> Triple(it.key, it.value, deck2Value) } }
+                    .filter{ (_, value1, value2) -> value1 != value2 }
+                    .forEach { (card, value1, value2) ->
+                        row("$value1 $card", "$value2 $card")
+                    }
             }
+        })
+
     }
 }
 
